c026f13e51d3dd8a7ac37d08d18b78ddd7d961cd
yiouejv <yiouejv@126.com>
2021-12-18 23:42:12 +0800

    35 second ans

--- array/binary_search/35.cpp
+++ array/binary_search/35.cpp
@@ -54,3 +54,22 @@ public:
         return r + 1;
     }
 };
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size();
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target) {
+                return m;
+            } else if (nums[m] > target) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        return r;
+    }
+};
