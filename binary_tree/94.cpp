44fa7ae815d4ed5b3646f53c32948f0d1d59936f
yiouejv <yiouejv@126.com>
2022-02-06 21:08:40 +0800

    前中后序遍历

---
+++ binary_tree/94.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        Traversal(root, ans);
+        return ans;
+    }
+private:
+    void Traversal(TreeNode *root, vector<int> &ans) {
+        if (root == nullptr) {
+            return;
+        }
+        Traversal(root->left, ans);
+        ans.push_back(root->val);
+        Traversal(root->right, ans);
+    }
+};
